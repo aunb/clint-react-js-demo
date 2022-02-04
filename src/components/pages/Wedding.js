f4768924544f5beade3308c332c42ff90480dfe3
muhammad munib <muhammad.munib@moebotech.com>
2022-02-04 15:50:17 +0500

    push code remove extra code

--- src/components/pages/Wedding.js
+++ src/components/pages/Wedding.js
@@ -8,8 +8,11 @@ const Wedding = props => {
       <div className="row">
           <div className="col-md-12">
               <div className="offset-md-2 col-md-8">
-                  <h5 >{props.title}</h5><br />
-                  <img src="/img3.jpg" className="header-logo w-100" alt="sparkale" />
+                  <img src="/img3.jpg" className="header-logo w-100" alt="sparkale" /><br /><br />
+                  <h5 >{props.title}</h5>
+                  <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
               </div>
           </div>
     </div>    
