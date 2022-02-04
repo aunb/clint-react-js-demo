c419d0d7a3b8542ea02f768ad45a78695e11071f
muhammad munib <muhammad.munib@moebotech.com>
2022-02-04 15:23:06 +0500

    push code remove extra code

--- src/container/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import postReducer from "../reducers/postReducer";
-import userReducer from "../reducers/userReducer";
-
-export default combineReducers({
-  allposts:postReducer,
-  allusers:userReducer,
-});
